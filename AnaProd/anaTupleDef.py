80b09a24637e2ce6cf284ab01aaacec281137a56
acagnotta <antimo.cagnotta@cern.ch>
2025-05-06 17:49:47 +0200

    ditaujet string changed and bug fixed for getEff

--- AnaProd/anaTupleDef.py
+++ AnaProd/anaTupleDef.py
@@ -115,8 +115,8 @@ def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal
     dfw.Define(f"ExtraJet_ptRes", f"Jet_ptRes[ExtraJet_B1]")
     dfw.Define(f"ExtraJet_idx", f"CreateIndexes(ExtraJet_pt.size())")
     dfw.Define(f"ExtraJet_p4", f"GetP4(ExtraJet_pt, ExtraJet_eta, ExtraJet_phi, ExtraJet_mass, ExtraJet_idx)")
-    dfw.Define(f"ExtraJet_legType", f"return ROOT::RVec<Leg >(ExtraJet_pt.size(), Leg::jet);")
-
+    dfw.Define(f"ExtraJet_legType", f"return ROOT::RVec<Leg>(ExtraJet_pt.size(), Leg::jet);")
+    dfw.Define(f"ExtraJet_decayMode", f"return ROOT::RVec<int>(ExtraJet_pt.size(), -2);") # dummy values
 
     if global_params["storeExtraJets"]:
         dfw.DefineAndAppend(f"ExtraJet_pt", f"v_ops::pt(Jet_p4[ExtraJet_B1])")
@@ -248,7 +248,7 @@ def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal
         dfw.Define( f"tau{leg_idx+1}_index", f"HttCandidate.leg_type.size() > {leg_idx} ? HttCandidate.leg_index.at({leg_idx}) : -1")
         dfw.Define( f"tau{leg_idx+1}_type", f"HttCandidate.leg_type.size() > {leg_idx} ? static_cast<int>(HttCandidate.leg_type.at({leg_idx})) : -1")
         # dfw.df.Print(f"tau{leg_idx+1}_type").Print()    
-        dfw.Define(f"b{leg_idx+1}_idx", f"Hbb_isValid ? HbbCandidate->leg_index[{leg_idx}] : -1")
+        dfw.Define(f"b{leg_idx+1}_index", f"Hbb_isValid ? HbbCandidate->leg_index[{leg_idx}] : -1")
         dfw.DefineAndAppend(f"b{leg_idx+1}_ptRes",f"Hbb_isValid ? static_cast<float>(Jet_ptRes.at(HbbCandidate->leg_index[{leg_idx}])) : 0.f")
         dfw.DefineAndAppend(f"b{leg_idx+1}_pt", f"Hbb_isValid ? static_cast<float>(HbbCandidate->leg_p4[{leg_idx}].Pt()) : 0.f")
         dfw.DefineAndAppend(f"b{leg_idx+1}_pt_raw", f"Hbb_isValid ? static_cast<float>(Jet_pt.at(HbbCandidate->leg_index[{leg_idx}])) : 0.f")
@@ -259,6 +259,7 @@ def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal
         dfw.Define(f"b{leg_idx+1}_p4", f"Hbb_isValid ? HbbCandidate->leg_p4[{leg_idx}] : LorentzVectorM()")
 
         dfw.Define(f"b{leg_idx+1}_legType", f"Hbb_isValid ? (Leg::jet) : (Leg::none)")
+        dfw.Define(f"b{leg_idx+1}_decayMode", f"-2") # dummy values
         if not isData:
             dfw.Define(f"b{leg_idx+1}_genJet_idx", f"Hbb_isValid ?  Jet_genJetIdx.at(HbbCandidate->leg_index[{leg_idx}]) : -1")
             for var in [ 'pt', 'eta', 'phi', 'mass' ]:
@@ -273,5 +274,5 @@ def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal
     if trigger_class is not None:
         hltBranches = dfw.Apply(trigger_class.ApplyTriggers, offline_legs, isData, applyTriggerFilter)
         dfw.colToSave.extend(hltBranches)
-    for leg_name in lepton_legs:
+    for leg_name in offline_legs:
         dfw.Redefine(f"{leg_name}_legType", f"static_cast<int>({leg_name}_legType)")
